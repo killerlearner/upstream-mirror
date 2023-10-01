ed8747b6b70bec269f7d23b7a5e35ec1a289fd1c
⎠⎠ THE༒KILLER ⎝⎝ <67904798+killerlearner@users.noreply.github.com>
2023-10-01 15:26:16 +0530

    Update bitsearch.py

--- engines/bitsearch.py
+++ engines/bitsearch.py
@@ -95,7 +95,7 @@ class bitsearch(object):
         if category is not None:
             params.append(('cat', ','.join(category)))
         params = urlencode(params)
-        jacket_url = self.url + "/api/v2.0/indexers/bitsearch/results/torznab/api?%s" % params
+        jacket_url = self.url + "/api/v2.0/indexers/badasstorrents/results/torznab/api?%s" % params
         response = self.get_response(jacket_url)
         if response is None:
             self.handle_error("connection error", what)
